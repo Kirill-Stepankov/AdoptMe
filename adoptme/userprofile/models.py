abd917b04ec0fe67e9b9dceec0f6cb6336f0eff6
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-08-07 21:07:57 +0300

    add pet model

--- adoptme/userprofile/models.py
+++ adoptme/userprofile/models.py
@@ -2,15 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-class Profile(models.Model):
-    class SexChoices(models.TextChoices):
+class SexChoices(models.TextChoices):
         MALE = 'M', 'Male'
         FEMALE = 'F', 'Female'
 
-    
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     about = models.TextField(blank=True)
     sex = models.CharField(max_length=1, choices=SexChoices.choices, default=SexChoices.MALE)
 
@@ -25,13 +26,38 @@ class Profile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.create(user=instance, slug=instance.username)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
+class Pet(models.Model):
+    class TypeChoices(models.TextChoices):
+        CAT = 'CAT', 'Cat'
+        DOG = 'DOG', 'Dog'
+        HORSE = 'HRS', 'Horse'
+        BIRD = 'BRD', 'Bird'
+        EXOTIC = 'EXC', 'Exotic'
 
+    class SizeChoices(models.TextChoices):
+        SMALL = 'SML', 'Small'
+        MEDIUM = 'MDM', 'Medium'
+        LARGE = 'LRG', 'Large'
+        EXTRA_LARGE = 'XXL', 'Extra large'
+
+    name = models.CharField(max_length=30)
+    about = models.TextField(blank=True)
+    gender = models.CharField(max_length=1, choices=SexChoices.choices, default=SexChoices.MALE)
+    type = models.CharField(max_length=3, choices=TypeChoices.choices, default=TypeChoices.CAT)
+    size = models.CharField(max_length=3, choices=SizeChoices.choices, default=SizeChoices.MEDIUM)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner')
+    age = models.IntegerField(default=1, validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ])
+    breed = models.CharField(max_length=100)
+    
 
 
     
