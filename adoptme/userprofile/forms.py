dab6e25dd646be44b24ecbf9c0e8802a725e84aa
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-08-13 19:52:46 +0300

    changing a bit frontend part

--- adoptme/userprofile/forms.py
+++ adoptme/userprofile/forms.py
@@ -81,3 +81,13 @@ class PetAdvertPhotoForm(forms.ModelForm):
             'pet_advert'
         ]
         widgets = {'pet_advert': forms.HiddenInput()}
+    
+    def clean_photo(self):
+       picture = self.cleaned_data.get("photo")
+       if not picture:
+           raise forms.ValidationError("No image!")
+       else:
+           w, h = get_image_dimensions(picture)
+           if w != h:
+               raise forms.ValidationError("The image must be square")
+       return picture
