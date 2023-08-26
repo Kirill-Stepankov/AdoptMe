ace4063519f2ed811c9b175c2dda3d04fd7713e1
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-08-27 01:28:32 +0300

    er diagram

--- adoptme/adoptme/settings.py
+++ adoptme/adoptme/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     "crispy_bootstrap5",
+    'django_extensions',
     'userprofile.apps.UserprofileConfig',
     'shelter.apps.ShelterConfig',
     'petadvert.apps.PetadvertConfig',
